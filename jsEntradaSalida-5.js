5ba7b5c90786477b69dd33bf87f2ccca0d4b780c
gregnaninl <gregnanin.leandro@gmail.com>
2017-02-02 22:13:37 -0300

    Add files via upload

---
+++ jsEntradaSalida-5.js
@@ -0,0 +1,16 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function Mostar()
+{	
+	
+  var Nombre;
+  var Edad;
+  Nombre= document.getElementById('elNombre').value;
+  Edad= document.getElementById('laEdad').value;
+  alert("Usted se llama "+Nombre+" y tiene "+Edad+" años");
+
+
+
+
+}
+
