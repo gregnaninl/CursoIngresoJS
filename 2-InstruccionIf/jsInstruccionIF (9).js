c982a89a444a8af2a5fc08f586e5f46f117df671
gregnaninl <gregnanin.leandro@gmail.com>
2017-02-09 22:11:44 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,5 +2,13 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+   var numeroRandom;
+
+
+  numeroRandom=Math.floor(Math.random() * 100) + 1;
+alert(numeroRandom);
+
+
+	
 
 }//FIN DE LA FUNCIÓN
