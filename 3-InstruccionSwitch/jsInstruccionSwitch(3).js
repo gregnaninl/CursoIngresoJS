430a2c9ea3cecf476ec7ffcffbf2e71f40efdbb6
gregnaninl <gregnanin.leandro@gmail.com>
2017-02-10 21:12:25 -0300

    Clase4

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,23 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño; 
+	mesDelAño= document.getElementById('mes').value;
 
-//alert (mesDelAño);
+	//alert (mesDelAño);
 	
-	
+	switch (mesDelAño)
+	{
+       
+    case "Febrero":
+       alert("Este mes NO tiene más de 29 días!!");
+        break;
+    default:      
+    alert("Este mes tiene 30 o más días");
+        break;  
+
+	}
+
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,10 +1,33 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+	//alert (mesDelAño);
 	
+	switch (mesDelAño)
+{
+    case "Enero":       
+    case "Abril":        
+    case "Junio":       
+    case "Agosto":                
+    case "Octubre":         
+    case "Diciembre":
+        alert("tiene 31 días!!!");
+        break;        
+    case "Febrero":
+        alert("tiene 28 días");
+        break;
+    case "Marzo":
+    case "Mayo":
+    case "Julio":
+    case "Septiembre":
+    case "Noviembre":
+        alert("tiene 30 días!");
+        break;
+   
+}
+
 	
 
 
