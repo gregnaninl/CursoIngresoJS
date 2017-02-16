8b8e6660c6c8e162c0c87b2343c535765db4fe96
gregnaninl <gregnanin.leandro@gmail.com>
2017-02-16 13:29:25 +0200

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -3,4 +3,28 @@ function Mostrar()
 	//Genero el número RANDOM entre 1 y 10 
 	
 
+numeroRandom=Math.floor(Math.random() * 10) + 1;
+//alert(numeroRandom);
+//console.log(numeroRandom);
+
+if(numeroRandom>8)
+{
+	alert("Su nota es "+numeroRandom+" EXCELENTE");
+	//document.getElementById('FormIngreso').value=numeroRandom+" EXCELENTE";
+}
+else
+{
+
+	if(numeroRandom<5)
+{
+	alert("Su nota es "+numeroRandom+" Vamos, la proxima se puede ");
+	//document.getElementById('FormIngreso').value=numeroRandom+" Vamos, la proxima se puede";
+}
+else
+{
+	alert(" Su nota es "+numeroRandom+" APROBÓ");
+	//document.getElementById('FormIngreso').value=numeroRandom+" APROBÓ";
+}
+}
+
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -4,6 +4,7 @@ function Mostrar()
 
 numeroRandom=Math.floor(Math.random() * 10) + 1;
 alert(numeroRandom);
+console.log(numeroRandom);
 
 
 
