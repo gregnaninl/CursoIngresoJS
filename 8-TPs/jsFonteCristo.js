1f1440baf04249d79e278dcc31490d85a93e62de
gregnaninl <gregnanin.leandro@gmail.com>
2017-02-23 17:08:37 -0300

    Add files via upload

--- 8-TPs/jsFonteCristo.js
+++ 8-TPs/jsFonteCristo.js
@@ -9,5 +9,178 @@ e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos
 */
 function ComenzarIngreso () 
 {
+ 		
+ 		var numero;
+ 		var numerosPares=numero;
+ 		var impar=numero;
+
+
+
+
+
+ 		numero=document.getElementById('numero').value;
+ 		numero=parseInt(numero);
+ 		NumerosPares=parseInt(NumerosPares);
+ 		impar=parseInt(impar);
+ 		while(numero<1)
+ 		{
+ 			numero=prompt("Por Favor inrese un numero Positivo");
+ 			numero=parseInt(numero);
+ 		 
+
+ 		}
  	
+ }	
+
+ function NumerosPares()
+ {
+ 		var numero;
+ 		var numerosPares;
+ 		
+ 		numero=document.getElementById('numero').value;
+ 		numero=parseInt(numero);
+ 		numerosPares=numero+2;
+ 		numerosPares=parseInt(numerosPares);
+ 		
+ 		while(numero<1)
+ 		{
+ 			numero=prompt("Por Favor inrese un numero Positivo");
+ 			numero=parseInt(numero);
+ 		 
+
+ 		}
+ 		while(numerosPares!=0 && numero%2==0)
+ 		{
+ 			
+ 			numerosPares=numerosPares-2;
+ 			document.write("numero"+numerosPares+"<BR>");
+ 				
+ 			
+ 		}
+ 		
+ }
+ 
+function NumerosImpares()
+{		
+	
+		var numero;
+ 		var impar=numero;
+ 		var divisor=100;
+
+
+ 		numero=document.getElementById('numero').value;
+ 		numero=parseInt(numero);
+ 		impar=numero+2;
+ 		impar=parseInt(impar);
+ 		while(numero<1)
+ 		{
+ 			numero=prompt("Por Favor inrese un numero Positivo");
+ 			numero=parseInt(numero);
+ 		}
+ 		while(impar!=1 && numero%2!=0)
+ 		{
+ 			impar=impar-2;
+			document.write("numero"+impar+"<BR>");
+ 		}
+}		
+
+function NumerosDivisibles()
+
+{
+		var numero;
+		var divisor=100;
+		var divisible;
+		numero=document.getElementById('numero').value;
+ 		numero=parseInt(numero);
+
+
+		while(divisor!=0)
+		{
+			divisible=numero%divisor
+			if(divisible==0)
+			{	
+			document.write("numero"+divisor+"<BR>");
+			divisor--;
+			}
+		else
+		{
+
+			divisor--;
+		}
+		}
+}
+function VerificarPrimo()
+
+{
+		var numero;
+		var divisor;
+		var divisible;
+		var primo=0;
+		numero=document.getElementById('numero').value;
+ 		numero=parseInt(numero);
+ 		divisor=numero;
+
+		while(divisor!=0)
+		{
+			divisible=numero%divisor
+			if(divisible==0)
+			{	
+			divisor--;
+			primo++;
+			}
+		else
+		{
+
+			divisor--;
+		}
+		
+		}
+
+		if(primo ==1)
+		{
+			alert("Es un numero Primo");
+		}
+		else
+		{
+			alert("No es un numero Primo");
+		}
+
+}
+
+function NumerosPrimos()
+
+{
+		var numero;
+		var divisor;
+		var divisible;
+		var primo=0;
+		var Primos;
+		numero=document.getElementById('numero').value;
+ 		numero=parseInt(numero);
+ 		divisor=numero;
+ 		primos;numero;
+
+		while(divisor!=0)
+		{
+			divisible=numero%divisor
+			if(divisible==0)
+			{	
+			divisor--;
+			primo++;
+			}
+		else
+		{
+
+			divisor--;
+		}
+
+		}
+		while(primo==1)
+		{
+
+			
+		}
+
+		
+		
 }
