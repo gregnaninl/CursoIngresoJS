d70f74d5cc401a7cfe2810d561bd2a851a62865f
gregnaninl <gregnanin.leandro@gmail.com>
2017-02-23 22:18:04 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,89 @@
 function Mostrar()
 {
+////prueba
+	/*var cont=0;
+
+	for(;;) en el for es importante que aiempre se agregen las ; adentro del ()
+	{
+	console.log(cont);	
+	cont++;
+	if (cont==5)  e
+	{
+		break;
+	}
+	}*/
+
+
+	var cont=0;
+	var cont1=0;
+	var cont2=0;
+	var cont3=0;
+	var cont4=0;
+	var cont5=0;
+	var cont6=0;
+	var cont7=0;
+	var cont8=0;
+	var cont9=0;
+	var cont10=0;
+
+	for(;cont<100000;cont++)
+	{
+
+	numeroRandom=Math.floor(Math.random() * 10) + 1;
+	//console.log(numeroRandom);
+	switch(numeroRandom)
+	{
+		case 1:
+		cont1++;
+		break;
+		case 2:
+		cont2++;
+		break;
+		case 3:
+		cont3++;
+		break;
+		case 4:
+		cont4++;
+		break;
+		case 5:
+		cont5++;
+		break;
+		case 6:
+		cont6++;
+		break;
+		case 7:
+		if(cont7==7000) // no dejo que pase mas del 7%
+		{
+			continue;
+		}
+		cont7++;
+		break;
+		case 8:
+		cont8++;
+		break;
+		case 9:
+		cont9++;
+		break;
+		case 10:
+		cont10++;
+		break;
+	}
+
+	}
+
+
+	console.log("La cantidad de 1 es "+cont1/cont*100+"%");
+	console.log("La cantidad de 2 es "+cont2/cont*100+"%");
+	console.log("La cantidad de 3 es "+cont3/cont*100+"%");
+	console.log("La cantidad de 4 es "+cont4/cont*100+"%");
+	console.log("La cantidad de 5 es "+cont5/cont*100+"%");
+	console.log("La cantidad de 6 es "+cont6/cont*100+"%");
+	console.log("La cantidad de 7 es "+cont7/cont*100+"%");
+	console.log("La cantidad de 8 es "+cont8/cont*100+"%");
+	console.log("La cantidad de 9 es "+cont9/cont*100+"%");
+	console.log("La cantidad de 10 es "+cont10/cont*100+"%");
+	console.log("cantidad de vueltas " +cont);
+	contTotal=(cont1+cont2+cont3+cont4+cont5+cont6)
+	console.log(contTotal);
 
 }
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,92 @@
 function Mostrar()
 {
 
+	var mascota;
+	var mascotaSi=0;
+	var mascotaNO=0;
+	var tipoDeMascota;
+	var contador=0;
+	var edad;
+	var edadMasVieja=edad;
+	var contadorDeGatos=0;
+
+
+	for(  ;contador<10;  )
+	{
+
+
+
+
+
+	sexo=prompt("ingrese sexo");
+	while(sexo!= "s" && sexo!="n")
+	{
+	sexo=prompt("ingrese sexo F o M");
+	}
+	edad=prompt("ingrese edad");
+	edad=parseInt(edad);
+	while(contador)	
+
+
+	mascota=prompt("¿Tiene mascota?");
+	while( mascota!="s" && mascota!="n")
+	{
+	mascota=prompt("tiene que responder por si o no!!");
+	}
+	
+	if(mascota=="s")
+	{
+	mascotaSi++;
+	tipoDeMascota=prompt("tiene perro o gato");
+	while(tipoDeMascota != "perro" && tipoDeMascota!= "gato")
+	{
+	tipoDeMascota=prompt("tiene que responder perro o gato");
+	}
+	if(tipoDeMascota=="gato")
+	{
+
+	nombre=prompt("ingrese el nombre del gato");
+	edadDelGato=prompt("ingrese edad del Gato");
+	while(edadDelGato<0 || edadDelGato>15)
+	{
+	edadDelGato=prompt("ingrese edad del Gato entre 0 y 15 años");
+	}
+	if(contadorDeGatos==0)
+	{
+		edadDelGato=edadDelGato
+	}
+
+	
+
+	else
+	{
+	mascotaNO++;
+	}
+	edad=prompt("ingrese su edad");
+	edad=parseInt(edad);
+	if(contador==0)
+	{
+	edadMasVieja=edad;
+	}
+	else
+	{
+	if(edad>edadMasVieja)
+	{
+	edadMasVieja=edad;
+	}
+
+	}
+///necesito saber el nombre del primer gato que sea de una mujer mayor a 30 años
+///y el gato no tenga mas de 5 años de edad
+
+
+
+
+
+
+	contador++;
+	}
+
+
 
 }
